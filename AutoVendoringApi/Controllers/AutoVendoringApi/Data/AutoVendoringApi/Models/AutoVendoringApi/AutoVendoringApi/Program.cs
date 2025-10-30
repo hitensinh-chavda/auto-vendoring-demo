601a9b1d3032f05789385c5963319cf4603441d7
hitensinh-chavda <hitensinh19881988@gmail.com>
2025-10-30 13:10:13 +0530

    Added Program.cs file (API entry point)

---
+++ AutoVendoringApi/Controllers/AutoVendoringApi/Data/AutoVendoringApi/Models/AutoVendoringApi/AutoVendoringApi/Program.cs
@@ -0,0 +1,35 @@
+using AutoVendoringApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register repository
+builder.Services.AddScoped<AutoVendoringRepository>();
+
+// Enable CORS for frontend access
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+// Middleware pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
